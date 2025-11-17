c3a9d5471415ee12bf44016b1b9f4c623531a53d
kaua lima <limakauapablo@gmail.com>
2025-11-16 22:41:34 -0300

    coisa pa porra

--- Coded_Prototipo/script.js
+++ Coded_Prototipo/script.js
@@ -5,17 +5,51 @@ function logar() {
     let identificacao = document.querySelector("#identificacao").value
     let senha = document.querySelector("#senha").value
 
-    if (identificacao == "kaua" && senha == "1234"){
+    if (identificacao == "aluno" && senha == "1234") {
         window.location.href = "login_aluno.html";
     }
 }
 
-function hamburguer(){
+function hamburguer() {
     if (ativar_display == 0) {
         janela.style.display = "inherit";
         ativar_display = 1
-    }else{
+    } else {
         janela.style.display = "none";
         ativar_display = 0
     }
+}
+
+const mes = new Date().getMonth() + 1
+
+const calendario = document.querySelector(".cal")
+const criarmes = document.createElement('h4')
+criarmes.textContent = "Mês: " + mes
+calendario.appendChild(criarmes)
+const table = document.querySelector("#table")
+
+let day = 1
+
+function emptydays() {
+    
+}
+
+while (day <= 30) {
+    const criar_linha = document.createElement("tr")
+    criar_linha.className = "line"
+    for (let i = 0; i < 7; i++) {
+        const criar_coluna = document.createElement("td")
+        criar_coluna.textContent = day
+        criar_coluna.className = "column"
+        if(day == 13 || day == 20){
+            criar_coluna.style.backgroundColor = "orange"
+        }else if(day == 30) {
+            criar_coluna.style.backgroundColor = "lightgreen"
+        }
+        if (day <=30){
+           criar_linha.appendChild(criar_coluna) 
+        }
+        day += 1
+    }
+    table.appendChild(criar_linha)
 }
